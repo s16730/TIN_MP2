36c8be51147e7e188db55aa2304f4163bd2f26a7
Bartłomiej Mazurowski <b.mazurowski@direktpoint.pl>
2021-01-17 15:10:31 +0100

    Added bare views;

---
+++ src/frontend/src/views/user/UserRegisterView.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+
+export default Vue.extend({
+  name: "UserRegisterView"
+});
+</script>
+
+<style scoped
+       lang="scss"
+>
+
+</style>
